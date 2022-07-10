4f786da719abb9813216d96614216ad3aa1445bd
Marek Szydlowski <marek.szydlow@gmail.com>
2022-07-10 20:57:05 +0200

    Added controller

--- flight_reservation/reservation_system.py
+++ flight_reservation/reservation_system.py
@@ -3,17 +3,19 @@ import uuid
 
 from .database import Connector, Repository
 
+FIELD_ROW = "row"
+FIELD_COLUMN = "column"
+FIELD_IS_VACANT = "is_vacant"
+
 
 class ReservationSystem:
     def __init__(self, connector: Connector) -> None:
         self.repository = Repository(connector)
 
-        self._free_seats = {}
-
     def cancel_reservation(self, user_id: str, flight_id: str, reservation_id: str) -> bool:
         self.repository.delete_reservation(user_id, flight_id, reservation_id)
         reservation = self.repository.get_reservation(user_id, flight_id, reservation_id)
-        row, column = reservation["row"], reservation["column"]
+        row, column = reservation[FIELD_ROW], reservation[FIELD_COLUMN]
         self.repository.update_seat(flight_id, row, column, True)
         # usuń z tabeli reservation
         # zmień na wolne w tabeli siedzeń
@@ -23,7 +25,7 @@ class ReservationSystem:
     def reserve_flight_with_random_seat(self, user_id: str, flight_id: str) -> bool:
         free_seats_in_flight = self.repository.get_free_seats(flight_id)
         seat = random.choice(free_seats_in_flight)
-        row, column = seat["row"], seat["column"]
+        row, column = seat[FIELD_ROW], seat[FIELD_COLUMN]
         self.repository.add_reservation(user_id, flight_id, row, column)
         self.repository.update_seat(flight_id, row, column, True)
 
@@ -32,23 +34,25 @@ class ReservationSystem:
     def reserve_flight_with_specific_seat(
         self, user_id: str, flight_id: str, row: int, column: int
     ) -> bool:
-        self.repository.add_reservation(user_id, flight_id, row, column)
-        self.repository.update_seat(flight_id, row, column, True)
-
-        return True
+        if self.repository._seat_is_free(flight_id, row, column):
+            self.repository.add_reservation(user_id, flight_id, row, column)
+            self.repository.update_seat(flight_id, row, column, True)
+            return True
+        else:
+            return False
 
     def _seat_is_free(self, flight_id: str, row: int, column: int) -> bool:
         flight_id = uuid.UUID(flight_id)
 
         free_seats = self.repository.get_free_seats(flight_id)
         for seat in free_seats:
-            seat_row, seat_column = seat["row"], seat["column"]
+            seat_row, seat_column = seat[FIELD_ROW], seat[FIELD_COLUMN]
             if row == seat_row and column == seat_column:
-                return seat["is_vacant"]
+                return seat[FIELD_IS_VACANT]
 
         return False
 
-    def _user_has_reservation_for_flight(self, user_id: str, flight_id: str) -> bool:
+    def _check_user_reservations(self, user_id: str, flight_id: str) -> bool:
         # TODO: Implement
         pass
 
