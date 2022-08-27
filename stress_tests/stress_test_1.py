06d1875be7c91c91543708d83afc706c0f0879d6
Marek Szydlowski <marek.szydlow@gmail.com>
2022-08-28 00:51:39 +0200

    Improved stress_test_1 and created stress_test_2

--- stress_tests/stress_test_1.py
+++ stress_tests/stress_test_1.py
@@ -1,25 +1,54 @@
 import random
 
+from definitions import NO_COLS, NO_ROWS
 from flight_reservation.database import Connector
 from flight_reservation.database.repository import Repository
+from flight_reservation.model.flight import Flight
+from flight_reservation.model.user import User
 from flight_reservation.reservation_system import ReservationSystem
+from flight_reservation.user_factory import UserFactory
 
 
-def main(reservationsystem, repository, name, last_name, row, column):
-    user = repository.create_new_user(name=name, last_name=last_name)
-    flights = reservationsystem.get_all_flights()
-    flight_id = random.choice(flights)
-    for i in range(100):
-        reservationsystem.reserve_flight_with_specific_seat(user.id, flight_id, row, column)
-    return True
+def main(reservation_system: ReservationSystem, user: User, flight: Flight) -> None:
+    row_ind, col_ind = random.randint(1, NO_ROWS - 1), random.randint(1, NO_COLS - 1)
+
+    for _ in range(100):
+        reserved = reservation_system.reserve_flight_with_specific_seat(
+            user.id, flight.id, row_ind, col_ind
+        )
+        if reserved:
+            print(f"Reserved seat: {row_ind} {col_ind}")
+        else:
+            print(f"Could not reserve seat: {row_ind} {col_ind}!")
+
+
+def get_random_flight(reservation_system: ReservationSystem) -> Flight:
+    return random.choice(reservation_system.get_all_flights())
 
 
 if __name__ == "__main__":
+    print("Initializing resources...")
     connector = Connector()
+    user_factory = UserFactory()
     repository = Repository(connector)
-    reservationsystem = ReservationSystem(connector)
+    reservation_system = ReservationSystem(connector)
+
+    user = user_factory.create_user(name="Stress", last_name="User")
+    repository.add_user(user.id, user.first_name, user.last_name)
+
+    flight = get_random_flight(reservation_system)
+
+    print(f"Created user: {user.id} - {user.first_name} {user.last_name}")
+    print(f"Selected flight with id: {flight.id}")
+
     try:
-        main(reservationsystem, repository, "John", "Doe", 7, 2)
+        print("Running test!")
+        main(reservation_system, user, flight)
     finally:
+        print("Cleaning up...")
+        repository.clean_seats(flight.id)
+        repository.clean_reservations()
+        repository.delete_user(user.id)
+
         connector.close()
         print("Stress test done...")
--- stress_tests/stress_test_2.py
+++ stress_tests/stress_test_2.py
@@ -1,33 +1,67 @@
 import random
+from multiprocessing import Process
+from typing import List
 
+from definitions import NO_COLS, NO_ROWS
 from flight_reservation.database import Connector
 from flight_reservation.database.repository import Repository
+from flight_reservation.model.flight import Flight
+from flight_reservation.model.user import User
 from flight_reservation.reservation_system import ReservationSystem
+from flight_reservation.user_factory import UserFactory
 
 
-def main(reservationsystem, repository, name, last_name, row, column):
-    user = repository.create_new_user(name=name, last_name=last_name)
-    for action in range(100):
-        r = random.random()
-        if r < 0.33:
-            reservationsystem.get_all_flights()
-        elif r < 0.66:
-            # flight id somehow
-            reservationsystem.reserve_flight_with_random_seat()
-        else:
-            continue
-            # if user has reservation cancel it
-            # else
-            # reservationsystem.reserve_flight_with_random_seat()
-    return True
+def execute_test(test_no):
+    def main(reservation_system: ReservationSystem, user: User, flights: List[Flight]) -> None:
+        for _ in range(200):
+            action_prob = random.random()
+            if action_prob > 0.70 and (
+                reservations := reservation_system.get_user_reservations(user.id)
+            ):
+                # Cancel reservation
+                reservation = random.choice(reservations)
+                print(f"USER_{test_no}: Cancelling reservation: {reservation.id} for seat {reservation.seat_row}, {reservation.seat_column}")
+                reservation_system.cancel_reservation(user.id, reservation.flight_id, reservation.id)
+            else:
+                # Seat reservation
+                flight = random.choice(flights)
+                row_ind, col_ind = random.randint(1, NO_ROWS - 1), random.randint(1, NO_COLS - 1)
+
+                reserved = reservation_system.reserve_flight_with_specific_seat(
+                    user.id, flight.id, row_ind, col_ind
+                )
+                if reserved:
+                    print(f"USER_{test_no}: Reserved seat: {row_ind}, {col_ind}")
+                else:
+                    print(f"USER_{test_no}: Could not reserve seat: {row_ind}, {col_ind}!")
+
+    print(f"USER_{test_no}: Initializing resources...")
+    connector = Connector()
+    user_factory = UserFactory()
+    repository = Repository(connector)
+    reservation_system = ReservationSystem(connector)
+
+    user = user_factory.create_user(name="Stress", last_name=f"User_{test_no}")
+    repository.add_user(user.id, user.first_name, user.last_name)
+
+    flights = reservation_system.get_all_flights()
+
+    print(f"USER_{test_no}: Created user: {user.id} - {user.first_name} {user.last_name}")
+
+    try:
+        print(f"USER_{test_no}: Running test!")
+        main(reservation_system, user, flights)
+    finally:
+        print(f"USER_{test_no}: Cleaning up...")
+        for flight in flights:
+            repository.clean_seats(flight.id)
+        repository.clean_reservations()
+        repository.delete_user(user.id)
+
+        connector.close()
+        print(f"USER_{test_no}: Stress test done...")
 
 
 if __name__ == "__main__":
-    connector = Connector()
-    repository = Repository(connector)
-    reservationsystem = ReservationSystem(connector)
-    try:
-        main(reservationsystem, repository, "John", "Doe", 7, 2)
-    finally:
-        connector.close()
-        print("Stress test done...")
+    for user_no in range(2):
+        Process(target=execute_test, args=(user_no,)).start()
